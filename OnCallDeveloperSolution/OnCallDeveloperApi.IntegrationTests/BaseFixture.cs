933593ce63c2828693b3111deaacceeebb9699cf
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-20 16:27:21 -0400

    api started

---
+++ OnCallDeveloperSolution/OnCallDeveloperApi.IntegrationTests/BaseFixture.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OnCallDeveloperApi.IntegrationTests;
+
+public class BaseFixture : IAsyncLifetime
+{
+    public IAlbaHost AlbaHost = null;
+    public async Task DisposeAsync()
+    {
+        await AlbaHost.DisposeAsync();
+    }
+
+    public async Task InitializeAsync()
+    {
+        AlbaHost = await Alba.AlbaHost.For<Program>(builder =>
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddMvcCore();
+            });
+        });
+    }
+}
---
+++ OnCallDeveloperSolution/OnCallDeveloperApi.IntegrationTests/GettingOnCallDeveloper.cs
@@ -0,0 +1,27 @@
+﻿
+namespace OnCallDeveloperApi.IntegrationTests;
+
+public class GettingOnCallDeveloper : IClassFixture<BaseFixture>
+{
+
+    private readonly IAlbaHost _host;
+
+    public GettingOnCallDeveloper(BaseFixture fixture)
+    {
+        _host = fixture.AlbaHost;
+    }
+
+    [Fact]
+    public async Task GettingTheOncallDeveloperAsync()
+    {
+        var result = await _host.Scenario(api =>
+        {
+            // first I want to do a get request to the resource
+            api.Get.Url("/"); // what host? what port? who cares!
+            api.StatusCodeShouldBeOk();
+            // I want to check the status code.
+        });
+
+
+    }
+}
