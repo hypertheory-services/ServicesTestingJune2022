f79bcc1f6dfdf856f90b2f67316de49ae5f36baa
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-21 13:00:08 -0400

    before refactor

--- OnCallDeveloperSolution/OnCallDeveloperApi.UnitTests/StandardBusinessClockTests.cs
+++ OnCallDeveloperSolution/OnCallDeveloperApi.UnitTests/StandardBusinessClockTests.cs
@@ -1,23 +1,43 @@
 ﻿
 
+using Moq;
+using OnCallDeveloperApi.Adapters;
 using OnCallDeveloperApi.Domain;
 
 namespace OnCallDeveloperApi.UnitTests;
 
 public class StandardBusinessClockTests
 {
-    [Fact]
-    public void DuringBusinessHoursClockReturnsTrue()
+    [Theory]
+    [InlineData(8,30,00)]
+    [InlineData(17,00,00)]
+    public void DuringBusinessHoursClockReturnsTrue(int hour, int minute, int second)
     {
-        IProvideTheBusinessClock clock = new StandardBusinessClock();
+        var stubbedClock = new Mock<ISystemTime>();
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, 6, 21, hour, minute, second));
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
 
         Assert.True(clock.IsBusinessHours());
     }
 
-    [Fact]
-    public void AfterBusinessHoursClockReturnsFalse()
+    [Theory]
+    [InlineData(8,29,59)]
+    public void AfterBusinessHoursClockReturnsFalse(int hour, int minute, int second)
     {
-        IProvideTheBusinessClock clock = new StandardBusinessClock();
+        var stubbedClock = new Mock<ISystemTime>();
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, 6, 21, hour, minute, second));
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+
+        Assert.False(clock.IsBusinessHours());
+    }
+
+    [Theory]
+    [InlineData(6,25)]
+    public void ClosedOnWeekends(int month, int day)
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        stubbedClock.Setup(c => c.GetCurrent()).Returns(new DateTime(2022, month, day, 11, 00, 00));
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
 
         Assert.False(clock.IsBusinessHours());
     }
--- OnCallDeveloperSolution/OnCallDeveloperApi/Domain/StandardBusinessClock.cs
+++ OnCallDeveloperSolution/OnCallDeveloperApi/Domain/StandardBusinessClock.cs
@@ -1,9 +1,37 @@
-﻿namespace OnCallDeveloperApi.Domain;
+﻿using OnCallDeveloperApi.Adapters;
+
+namespace OnCallDeveloperApi.Domain;
 
 public class StandardBusinessClock : IProvideTheBusinessClock
 {
+    private readonly ISystemTime _systemTime;
+
+    public StandardBusinessClock(ISystemTime systemTime)
+    {
+        _systemTime = systemTime;
+    }
+
     public bool IsBusinessHours()
     {
-        return true;
+        var now = _systemTime.GetCurrent();
+        if (IsTheWeekend(now))
+        {
+            return false;
+        }
+        return AfterStart(now) && BeforeClose(now);
+    }
+
+    private static bool AfterStart(DateTime now)
+    {
+        return now.Hour >= 8 && now.Minute >= 30;
+    }
+
+    private static bool BeforeClose(DateTime now)
+    {
+        return now.Hour >= 17 && now.Minute < 1;
+    }
+    private static bool IsTheWeekend(DateTime now)
+    {
+        return now.DayOfWeek == DayOfWeek.Sunday || now.DayOfWeek == DayOfWeek.Saturday;
     }
 }
