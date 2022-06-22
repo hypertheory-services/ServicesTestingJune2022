7a5fb7ba1c9e178e617140e5d0011e4f868e28ec
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-22 10:33:07 -0400

    buttresses still up

---
+++ Banking/Banking.Domain/UserAccountGenerator.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Banking.Domain;
+
+public class UserAccountGenerator
+{
+    private readonly IGenerateUserAccountSeeds _userAccountSeedGenerator;
+
+    public UserAccountGenerator(IGenerateUserAccountSeeds userAccountSeedGenerator)
+    {
+        _userAccountSeedGenerator = userAccountSeedGenerator;
+    }
+
+    // This is not good for NEW code
+    public UserAccountGenerator()
+    {
+        _userAccountSeedGenerator = new RandomUserAccountSeeder();
+    }
+
+    public string CreateUserAccount(string firstName, string lastName, int age)
+    {
+        // "bob-smith-13"
+        int num = GetRandomKey(age);
+
+        return $"{firstName.Trim().ToLower()}-{lastName.Trim().ToLower()}-{num}";
+    }
+
+    protected virtual int GetRandomKey(int age)
+    {
+        return _userAccountSeedGenerator.GetSeedFor(age);
+    }
+}
---
+++ Banking/Banking.UnitTests/GeneratingUserAccounts.cs
@@ -0,0 +1,43 @@
+﻿
+using Moq.Protected;
+
+namespace Banking.UnitTests;
+
+public class GeneratingUserAccounts
+{
+
+    [Theory]
+    [InlineData("Bob", "Smith", 38, "bob-smith-18")]
+    [InlineData("Jill", "Jones", 38, "jill-jones-18")]
+    public void CanGenerateUserAccounts(string first, string last, int age, string expected)
+    {
+        var generator = new ScaffoldedUserAccountGenerator();
+
+        var result = generator.CreateUserAccount(first, last, age);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("Bob", "Smith", 38, "bob-smith-0")]
+    [InlineData("Jill", "Jones", 38, "jill-jones-0")]
+    public void CanGenerateUserAccountsWithMock(string first, string last, int age, string expected)
+    {
+        var generator = new Mock<UserAccountGenerator>();
+        generator.Protected().Setup<int>("GetRandomKey", It.IsAny<int>()).Returns(0);
+        //var generator = new UserAccountGenerator();
+
+        var result = generator.Object.CreateUserAccount(first, last, age);
+
+        Assert.Equal(expected, result);
+    }
+
+}
+
+public class ScaffoldedUserAccountGenerator : UserAccountGenerator
+{
+    protected override int GetRandomKey(int age)
+    {
+        return 18;
+    }
+}
